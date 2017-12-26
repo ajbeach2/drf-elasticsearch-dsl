e8c3301d02d458351d8d94f1a6f3de04a3ddac6a
Alex Beach <ajbeach2@gmail.com>
2017-12-26 11:06:30 -0500

    settings

--- docs/settings.py
+++ docs/settings.py
@@ -26,19 +26,11 @@ INSTALLED_APPS = [
     "drf_elasticsearch_dsl",
 
 
-    "tests",
-    "django_celery_results",
+    "tests"
 ]
-
-CELERY_BROKER_URL = 'memory://'
-CELERY_RESULT_BACKEND = 'django-db'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
 
 DRF_SERIALIZER_ELASTICSERACH_SETTTINGS = {
     'elasticsearch_hosts': ['localhost'],
-    'signal_processor_class': 'drf_elasticsearch_dsl.signals.CelerySignalProcessor',
 }
 
 SITE_ID = 1
